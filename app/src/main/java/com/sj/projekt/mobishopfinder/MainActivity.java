381901cb51ebf22e5c06d9460b1b3acb7cc88cd6
Fehmi Citaku <fehmicitaku@gmail.com>
2018-03-18 23:52:15 +0100

    Fourth Commit - Implementimi i te gjitha clasave startuese sbashku me intent
    Kam shtuar disa CardView ne LauncherActivity te cilat jan funksionale, jan te lidhura me intent, qdo klik te qon ne nje aktivitet tjeter, ne CardView Zgjidh Qytetin, i cili eshte i lidhur me klasen Cities.class kam shtuar disa buttona per qdo qytet te Kosoves, ku perseri ne ta do te shtohen se paku 3 Mobile Shopa per secilin per qytetit,

--- app/src/main/java/com/sj/projekt/mobishopfinder/MainActivity.java
+++ app/src/main/java/com/sj/projekt/mobishopfinder/MainActivity.java
@@ -1,17 +1,53 @@
 package com.sj.projekt.mobishopfinder;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    private CardView zgjidh_qytetin, topten, lowHighPrice, becomeReseller;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        zgjidh_qytetin = findViewById(R.id.zgjidh_qytetin);
+        topten = findViewById(R.id.topten);
+        lowHighPrice = findViewById(R.id.lowHighPrice);
+        becomeReseller = findViewById(R.id.becomeReseller);
 
 
+        zgjidh_qytetin.setOnClickListener(this);
+        topten.setOnClickListener(this);
+        lowHighPrice.setOnClickListener(this);
+        becomeReseller.setOnClickListener(this);
+
+
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent i;
+
+        switch (view.getId()) {
+
+            case R.id.zgjidh_qytetin : i = new Intent(this, Cities.class);startActivity(i); break;
+            case R.id.topten : i = new Intent(this, topten.class);startActivity(i); break;
+            case R.id.lowHighPrice : i = new Intent(this, lowhigh.class);startActivity(i); break;
+            case R.id.becomeReseller : i = new Intent(this, becomereseller.class);startActivity(i); break;
+
+            default: break;
+
+
+
+        }
 
     }
 }
