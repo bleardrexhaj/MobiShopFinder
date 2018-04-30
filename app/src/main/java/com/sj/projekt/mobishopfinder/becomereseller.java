694b98337d821a3cc86d744442551f76b06a0362
edibytyqi <edibytyqi@live.com>
2018-04-30 23:09:18 +0200

    Dizajnimi i faqes per regjistrim te mobishopeve.

--- app/src/main/java/com/sj/projekt/mobishopfinder/becomereseller.java
+++ app/src/main/java/com/sj/projekt/mobishopfinder/becomereseller.java
@@ -1,13 +1,39 @@
 package com.sj.projekt.mobishopfinder;
 
+import android.content.Intent;
+import android.net.wifi.WifiManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.TextView;
 
-public class becomereseller extends AppCompatActivity {
+public class becomereseller extends AppCompatActivity  implements View.OnClickListener{
 
+    private TextView login_text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_becomereseller);
+
+
+        getWindow().setBackgroundDrawableResource(R.drawable.iph);
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN); //mos mu hap keyboardi(automatic)
+
+
+        login_text = (TextView) findViewById(R.id.login_text);
+
+        login_text.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+
+
+        Intent intent = new Intent(this,Signin.class);
+        startActivity(intent);
+
     }
 }
