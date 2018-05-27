b6884e6ba66ba7532b314c0d1549d44024229387
Fehmi Citaku <fehmicitaku@gmail.com>
2018-05-27 16:31:00 +0200

    Komiti i radhes  - Fehmi Citaku - Spinner
    Kam shtuar nje spinner per zgjedhjen e qytetit, po ashtu kam  implementuar edhe Listenerin per te krijuar aktivitetin ne qytetin qe e zgjedhim ne Spinner

--- app/src/main/java/com/sj/projekt/mobishopfinder/Cities.java
+++ app/src/main/java/com/sj/projekt/mobishopfinder/Cities.java
@@ -1,9 +1,12 @@
 package com.sj.projekt.mobishopfinder;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.sj.projekt.mobishopfinder.Qytetet.Artane;
@@ -33,78 +36,114 @@ public class Cities extends AppCompatActivity implements View.OnClickListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cities);
 
-        artane =  findViewById(R.id.artane);
-        besiane = findViewById(R.id.besiane);
-        decan = findViewById(R.id.decan);
-        ferizaj = findViewById(R.id.ferizaj);
-        fushekosove = findViewById(R.id.fushekosove);
-        gjakove = findViewById(R.id.gjakove);
-        gjilan = findViewById(R.id.gjilan);
-        kline = findViewById(R.id.kline);
-        malisheve = findViewById(R.id.malisheve);
-        mitrovice = findViewById(R.id.mitrovice);
-        peje = findViewById(R.id.peje);
-        prishtine = findViewById(R.id.prishtine);
-        prizren = findViewById(R.id.prizren);
-        rahovec = findViewById(R.id.rahovec);
-        skenderaj = findViewById(R.id.skenderaj);
-        shtime = findViewById(R.id.shtime);
-        therand = findViewById(R.id.therande);
-        viti = findViewById(R.id.viti);
-        vushtrri = findViewById(R.id.vushtrri);
 
-        artane.setOnClickListener(this);
-        besiane.setOnClickListener(this);
-        decan.setOnClickListener(this);
-        ferizaj.setOnClickListener(this);
-        fushekosove.setOnClickListener(this);
-        gjakove.setOnClickListener(this);
-        gjilan.setOnClickListener(this);
-        kline.setOnClickListener(this);
-        malisheve.setOnClickListener(this);
-        mitrovice.setOnClickListener(this);
-        peje.setOnClickListener(this);
-        prishtine.setOnClickListener(this);
-        prizren.setOnClickListener(this);
-        rahovec.setOnClickListener(this);
-        skenderaj.setOnClickListener(this);
-        shtime.setOnClickListener(this);
-        therand.setOnClickListener(this);
-        viti.setOnClickListener(this);
-        vushtrri.setOnClickListener(this);
+        String selection;
 
+
+        final Spinner mySpinner = findViewById(R.id.spinner1);
+
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(Cities.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.cities));
+        myAdapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        mySpinner.setAdapter(myAdapter);
+
+        mySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
+
+                String selection = (String) mySpinner.getSelectedItem();
+                final Intent intent;
+                if(selection.equals("Artane"))
+                {
+                    intent = new Intent(Cities.this, Artane.class);
+                    startActivity(intent);
+                }
+                else if(selection.equals("Besiane")) {
+                    intent = new Intent(Cities.this, Besiane.class);
+                    startActivity(intent);
+                }
+                else if(selection.equals("Decan")) {
+                    intent = new Intent(Cities.this, Decan.class);
+                    startActivity(intent);
+                }
+                else if(selection.equals("Ferizaj")) {
+                    intent = new Intent(Cities.this, Ferizaj.class);
+                    startActivity(intent);
+                }
+                else if(selection.equals("Fushe_Kosove")) {
+                    intent = new Intent(Cities.this, Fushe_Kosove.class);
+                    startActivity(intent);
+                }
+                else if(selection.equals("Gjakove")) {
+                    intent = new Intent(Cities.this, Gjakove.class);
+                    startActivity(intent);
+                }
+                else if(selection.equals("Gjilan")) {
+                    intent = new Intent(Cities.this, Gjilan.class);
+                    startActivity(intent);
+                }
+                else if(selection.equals("Kline")) {
+                    intent = new Intent(Cities.this, Kline.class);
+                    startActivity(intent);
+                }
+                else if(selection.equals("Malisheve")) {
+                    intent = new Intent(Cities.this, Malisheve.class);
+                    startActivity(intent);
+                }
+                else if(selection.equals("Mitrovice")) {
+                    intent = new Intent(Cities.this, Mitrovice.class);
+                    startActivity(intent);
+                }
+                else if(selection.equals("Peje")) {
+                    intent = new Intent(Cities.this, Peje.class);
+                    startActivity(intent);
+                }
+                else if(selection.equals("Prishtine")) {
+                    intent = new Intent(Cities.this, Prishtine.class);
+                    startActivity(intent);
+                }
+                else if(selection.equals("Prizren")) {
+                    intent = new Intent(Cities.this, Prizren.class);
+                    startActivity(intent);
+                }
+                else if(selection.equals("Rahovec")) {
+                    intent = new Intent(Cities.this, Rahovec.class);
+                    startActivity(intent);
+                }
+                else if(selection.equals("Skenderaj")) {
+                    intent = new Intent(Cities.this, Skenderaj.class);
+                    startActivity(intent);
+                }
+                else if(selection.equals("Shtime")) {
+                    intent = new Intent(Cities.this, Shtime.class);
+                    startActivity(intent);
+                }
+                else if(selection.equals("Therande")) {
+                    intent = new Intent(Cities.this, Therande.class);
+                    startActivity(intent);
+                }
+                else if(selection.equals("Viti")) {
+                    intent = new Intent(Cities.this, Viti.class);
+                    startActivity(intent);
+                }
+                else if(selection.equals("Vushtrri")) {
+                    intent = new Intent(Cities.this, Vushtrri.class);
+                    startActivity(intent);
+                }
+
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
 
         }
+
     @Override
     public void onClick(View view) {
-        Intent i;
 
-        switch (view.getId()) {
-
-            case R.id.artane : i = new Intent(this, Artane.class);startActivity(i); break;
-            case R.id.besiane : i = new Intent(this, Besiane.class);startActivity(i); break;
-            case R.id.decan : i = new Intent(this, Decan.class);startActivity(i); break;
-            case R.id.ferizaj : i = new Intent(this, Ferizaj.class);startActivity(i); break;
-            case R.id.fushekosove : i = new Intent(this, Fushe_Kosove.class);startActivity(i); break;
-            case R.id.gjakove : i = new Intent(this, Gjakove.class);startActivity(i); break;
-            case R.id.gjilan : i = new Intent(this, Gjilan.class);startActivity(i); break;
-            case R.id.kline : i = new Intent(this, Kline.class);startActivity(i); break;
-            case R.id.malisheve : i = new Intent(this, Malisheve.class);startActivity(i); break;
-            case R.id.mitrovice : i = new Intent(this, Mitrovice.class);startActivity(i); break;
-            case R.id.peje : i = new Intent(this, Peje.class).putExtra("Qyteti","Peje");startActivity(i); break;
-            case R.id.prishtine : i = new Intent(this, Prishtine.class);startActivity(i); break;
-            case R.id.prizren : i = new Intent(this, Prizren.class);startActivity(i); break;
-            case R.id.rahovec : i = new Intent(this, Rahovec.class);startActivity(i); break;
-            case R.id.skenderaj : i = new Intent(this, Skenderaj.class);startActivity(i); break;
-            case R.id.shtime : i = new Intent(this, Shtime.class);startActivity(i); break;
-            case R.id.therande : i = new Intent(this, Therande.class);startActivity(i); break;
-            case R.id.viti : i = new Intent(this, Viti.class);startActivity(i); break;
-            case R.id.vushtrri : i = new Intent(this, Vushtrri.class);startActivity(i); break;
-
-            default: break;
-
-
-
-        }
     }
 }
